d3ab1d9349e369e4268decea818ec75d553e14f9
Julian De Vita <julianmdevita@gmail.com>
2020-10-28 10:50:58 -0700

    Added testimonial section

--- precache-manifest.2e51b3879f5c1cc19830ef75dec3400e.js
+++ precache-manifest.2e51b3879f5c1cc19830ef75dec3400e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "417b8e20955d1d0ab4ae3d727f734101",
+    "revision": "8b5a7ca1a744012c9a60ca8259f6c8ff",
     "url": "/index.html"
   },
   {
-    "revision": "9130af87c5442646c811",
+    "revision": "5378ae9c18ddec6542d3",
     "url": "/static/css/2.d48510fc.chunk.css"
   },
   {
-    "revision": "4020aa9f5d282601bb45",
+    "revision": "70bcef35d41099487565",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "9130af87c5442646c811",
-    "url": "/static/js/2.aae9381d.chunk.js"
+    "revision": "5378ae9c18ddec6542d3",
+    "url": "/static/js/2.d2f7d93e.chunk.js"
   },
   {
-    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.aae9381d.chunk.js.LICENSE.txt"
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d2f7d93e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4020aa9f5d282601bb45",
-    "url": "/static/js/main.da823a92.chunk.js"
+    "revision": "70bcef35d41099487565",
+    "url": "/static/js/main.f870b30f.chunk.js"
   },
   {
     "revision": "73a4f215652a07459b34",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7250cca705301460d17c5ee8b036b78d.js"
+  "/precache-manifest.2e51b3879f5c1cc19830ef75dec3400e.js"
 );
 
 self.addEventListener('message', (event) => {
